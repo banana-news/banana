5a76e85dc2a3cad8ff41628a6321e32b0c5a6bed
Albert Kemp <91766342+albertkemp@users.noreply.github.com>
2024-02-18 17:11:49 +1100

    s

--- docs/scripts/share.js
+++ docs/scripts/share.js
@@ -11,3 +11,7 @@ function embedShare() {
     navigator.clipboard.writeText('<embed type="text/html" src="https://banana-news.github.io/reports"  width="600" height="400">');
     alert('Embed code copied.');
 }
+const downloadlink = document.getElementById('downloadlink');
+downloadlink.addEventListener("click", function(){
+    alert('Image downloaded. (It should be in your "Downloads" folder.)');
+});

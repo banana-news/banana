7b9bd35282193572ec16745497eaded0d8663974
Albert Kemp <91766342+albertkemp@users.noreply.github.com>
2024-02-18 17:01:54 +1100

    s

---
+++ docs/scripts/share.js
@@ -0,0 +1,13 @@
+//Max and Sinan, please ask me before editing or deleting this:
+function htmlShare() {
+    navigator.clipboard.writeText('<a href="https://banana-news.github.io/reports">https://banana-news.github.io/reports</a>');
+    alert('HTML code copied.');
+}
+function linkCopy() {
+    navigator.clipboard.writeText('https://banana-news.github.io/reports');
+    alert('Link copied.');
+}
+function embedShare() {
+    navigator.clipboard.writeText('<embed type="text/html" src="https://banana-news.github.io/reports"  width="600" height="400">');
+    alert('Embed code copied.');
+}

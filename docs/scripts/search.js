06b9cda456ec07435b09d5bd20ad848e747db1bc
Albert Kemp <91766342+albertkemp@users.noreply.github.com>
2024-02-24 09:19:14 +1100

    s

--- docs/scripts/search.js
+++ docs/scripts/search.js
@@ -1,6 +1,12 @@
+var searchbar = document.getElementById('searchbar');
+searchbar.addEventListener("keypress", function(event){
+    if (event.key =='Enter') {
+        search();
+    }
+});
 function search() {
     var searchbar = document.getElementById("searchbar").value;
-    if (searchbar === "") {
+    if (searchbar == "") {
         alert("Please enter a search term");
     } else {
         window.location.href = "searchresults.html?q=" + encodeURIComponent(searchbar);

b3e222311a06b46ce4467da7a5630aa90b3b6ed7
Albert Kemp <91766342+albertkemp@users.noreply.github.com>
2024-02-24 09:55:37 +1100

    s

--- docs/scripts/search.js
+++ docs/scripts/search.js
@@ -1,3 +1,25 @@
+/*Max and Sinan, please ask Albert before editing or deleting this code.*/
+/*
+[]
+ []
+ []]]
+ []]]]]]
+  []]]]]]]
+   []]]]]]]]
+     []]]]]]]]
+       []]]]]]]]]]
+         []]\]]]]]]]]]]
+           []]]]]]]]]
+|
+|___     ____      ___     ____      ___     ____            ___     ___               __
+|   \   /   |   | /   \   /   |   | /   \   /   |         | /   \   /___\   |  |   |  (__ 
+|___/   \__/ \  |/    |   \__/ \  |/    |   \__/ \        |/    |   \____   \__/\__/   __)
+
+If you're the sort of person who looks at the source code of webpages, try our challenge:
+
+https://banana-news.github.io/reports/share_this_page.html
+
+*/
 var searchbar = document.getElementById('searchbar');
 searchbar.addEventListener("keypress", function(event){
     if (event.key =='Enter') {

45580c8163e8e3dbc1844e1469386e7ff418ff7d
Albert Kemp <91766342+albertkemp@users.noreply.github.com>
2024-03-03 17:42:56 +1100

    s

--- docs/scripts/elonmoney.js
+++ docs/scripts/elonmoney.js
@@ -41,49 +41,50 @@ https://banana-news.github.io/reports/share_this_page.html
  |_____________________________________________________________________________________________________________________|
 
  */
-const moneyDisplay = document.getElementById('moneyDisplay');
-let money = 213000000000;
-const receiptPrice = 0;
-const receiptItems = 0;
-const receiptDiv = document.getElementById('receiptDiv');
-
-const bigMac = 5;
-const pizza = 15;
-const book = 20;
-const parrot = 1000;
-const michelinRestaurant = 2000;
-const volksGolf = 40000;
-const tesla = 150000;
-const porsche = 200000;
-const formulaOne = 13000000;
-const airbus = 450000000;
-const house = 350000;
-const mansion = 1800000;
-const whiteHouse = 400000000;
-const burjKhalifa = 2000000000;
-const unitedAirlines = 30000000000;
-const sheratonGrand = 100000000;
-
-
-//etc
-
-function buyItem(item) {
-    money= money-item;
-    receiptPrice = receiptPrice+item;
-    receiptItems++;
-}
-function sellItem(item) {
-    money = money+item;
-    receiptPrice=receiptPrice-item;
-    receiptItems--;
-}
-
-moneyDisplay.innerHTML = money;
-
-if (money <=0) {
- money = 0;
-}
-
-function showReceipt() {
-  receiptDiv.innerHTML = '<p><b>Your Receipt:</b><br>Items: ' + receiptItems + '<br><br>Total: ' + receiptPrice + '</p>';
-}
+ const moneyDisplay = document.getElementById('moneyDisplay');
+ let money = 213000000000;
+ let receiptPrice = 0;
+ let receiptItems = 0;
+ const receiptDiv = document.getElementById('receiptDiv');
+ 
+ const bigMac = 5;
+ const pizza = 15;
+ const book = 20;
+ const parrot = 1000;
+ const michelinRestaurant = 2000;
+ const volksGolf = 40000;
+ const tesla = 150000;
+ const porsche = 200000;
+ const formulaOne = 13000000;
+ const airbus = 450000000;
+ const house = 350000;
+ const mansion = 1800000;
+ const whiteHouse = 400000000;
+ const burjKhalifa = 2000000000;
+ const unitedAirlines = 30000000000;
+ const sheratonGrand = 100000000;
+ 
+ 
+ //etc
+ 
+ function buyItem(item) {
+     money= money-item;
+     receiptPrice = receiptPrice+item;
+     receiptItems++;
+     moneyDisplay.innerHTML = money;
+ }
+ function sellItem(item) {
+     money = money+item;
+     receiptPrice=receiptPrice-item;
+     receiptItems--;
+     moneyDisplay.innerHTML = money;
+ }
+ 
+ if (money <=0) {
+  money = 0;
+ }
+ 
+ function showReceipt() {
+   receiptDiv.innerHTML = '<p><b>Your Receipt:</b><br>Items: ' + receiptItems + '<br><br>Total: ' + receiptPrice + '</p>';
+ }
+ 

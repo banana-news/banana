18da98f189f8e253258c1218ae106148256903ed
Albert Kemp <91766342+albertkemp@users.noreply.github.com>
2024-03-10 08:05:28 +1100

    s

--- docs/scripts/searchresults.js
+++ docs/scripts/searchresults.js
@@ -58,7 +58,9 @@ var searchResultsDiv = document.getElementById("searchResults");
 var results = [];
 
 function tiltPage() {
-    document.body.style.rotate = '4deg';
+    document.body.style.rotate = '2deg';
+    document.body.style.top = '20px';
+    document.body.style.position='absolute';
 }
 
 if (searchQuery.toLowerCase().includes('home') || searchQuery.toLowerCase().includes('index') || searchQuery.toLowerCase().includes('main') || searchQuery.toLowerCase().includes('banana') || searchQuery.toLowerCase().includes('stuff') || searchQuery.toLowerCase().includes('all') || searchQuery.toLowerCase().includes('page') || searchQuery.toLowerCase().includes('every')) {

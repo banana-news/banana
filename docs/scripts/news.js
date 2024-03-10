c6b31792b388baeb0cbbd0b0c2c854f0d748e2b6
Albert Kemp <91766342+albertkemp@users.noreply.github.com>
2024-03-11 10:22:24 +1100

    s

--- docs/scripts/news.js
+++ docs/scripts/news.js
@@ -1,3 +1,5 @@
+const hidden1  =document.getElementById('hidden1');
+const hidden2 = document.getElementById('hidden2');
 function revealText(text, button) {
     text.style.display='block';
     button.style.display='none';

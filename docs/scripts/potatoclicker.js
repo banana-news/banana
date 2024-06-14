6fd4884a4850f339b2b2e1d0807eaff31d7be200
Albert Kemp <91766342+albertkemp@users.noreply.github.com>
2024-06-14 16:56:31 +1000

    a

--- docs/scripts/potatoclicker.js
+++ docs/scripts/potatoclicker.js
@@ -42,7 +42,6 @@ var secondsOn = false;
 
 var plusOne = document.getElementById("+1");
 var perSec = document.getElementById("persec");
-var intervalId; // To keep track of the interval
 
 function addOne() {
     numClicks++;
@@ -50,110 +49,106 @@ function addOne() {
 }
 
 function updateDisplay() {
-    // Update the display of clicks
-    var text = numClicks === 1 ? " banana" : " bananas";
-    $("#results").text(numClicks + text);
+    if (numClicks == 1) {
+        $("#results").text(numClicks + " banana");
+    } else {
+        $("#results").text(numClicks + " bananas");
+    }
+
+    if (numClicks >= plusOnePrice) {
+        plusOn = true;
+        plusOne.removeClass("locked").addClass("unlocked");
+        $("#locked5").css("display", "none");
+    }
+
+    if (numClicks >= plusSecPrice) {
+        secOn = true;
+        perSec.removeClass("locked").addClass("unlocked");
+        $("#locked6").css("display", "none");
+    }
 }
 
-plusOne.addEventListener("click", function() {
-    if (plusOn) {
+plusOne.addEventListener("click", function () {
+    if (plusOn && numClicks >= plusOnePrice) {
         addingOn = true;
         numClicks -= plusOnePrice;
+        plusOnePrice += 100;  // Increment price for the next upgrade
         updateDisplay();
     }
 });
 
-perSec.addEventListener("click", function() {
-    if (secOn) {
+perSec.addEventListener("click", function () {
+    if (secOn && numClicks >= plusSecPrice) {
         secondsOn = true;
         numClicks -= plusSecPrice;
+        plusSecPrice += 100;  // Increment price for the next upgrade
+        setInterval(addOne, 1000);
         updateDisplay();
     }
 });
 
-$("#potato").on("click", function() {
+$("#potato").on("click", function () {
     if (!addingOn) {
         addOne();
-    } else if (addingOn) {
+    } else {
         numClicks += 2;
         updateDisplay();
     }
-    if (secondsOn && !intervalId) {
-        intervalId = setInterval(addOne, 1000);
+
+    if (numClicks >= 100) {
+        if (!$("#cookie").hasClass("unlocked")) {
+            alert("You unlocked a slice of banana bread!");
+            $("#locked1").css("display", "none");
+            $("#cookie").removeClass("locked").addClass("unlocked");
+            var image = $("<img>");
+            image.attr("width", imageWidth);
+            image.attr("src", "https://static01.nyt.com/images/2023/09/28/multimedia/LH-banana-bread-hkbj/LH-banana-bread-hkbj-square640.jpg");
+            image.appendTo("#cookie");
+        }
     }
-            if (secondsOn){
-                setInterval(addOne, 1000);
-            }
-            if (numClicks==1){
-                $("#results").text(numClicks + " banana");
-            }else{
-            $("#results").text(numClicks + " bananas");
-            }
-            if (numClicks>=plusOnePrice){
-                plusOn=true;
-                plusOnePrice+=100;
-                plusOne.removeClass("locked").addClass("unlocked");
-                $("#locked5").css("display", "none");
-            }
-            if (numClicks>=plusSecPrice){
-                secOn=true;
-                plusSecPrice+=100;
-                perSec.removeClass("locked").addClass("unlocked");
-                $("#locked6").css("display", "none");
-            }
-            if (numClicks >= 100) {
-                if (!$("#cookie").hasClass("unlocked")) {
-                    alert("You unlocked a slice of banana bread!");
-                    $("#locked1").css("display", "none");
-                    $("#cookie").removeClass("locked").addClass("unlocked");
-                    var image = $("<img>");
-                    image.attr("width", imageWidth);
-                    image.attr("src", "https://static01.nyt.com/images/2023/09/28/multimedia/LH-banana-bread-hkbj/LH-banana-bread-hkbj-square640.jpg");
-                    image.appendTo("#cookie");
-                }
-            }
-            if (numClicks >= 250) {
-                if (!$("#crocpet").hasClass("unlocked")) {
-                    alert("You unlocked golden banana!");
-                    $("#locked2").css("display", "none");
-                    $("#crocpet").removeClass("locked").addClass("unlocked");
-                    var image = $("<img>");
-                    image.attr("width", imageWidth);
-                    image.attr("src", "https://media.artsper.com/artwork/2181373_1_m.jpg");
-                    image.appendTo("#crocpet");
-                }
-            }
-            if (numClicks >= 500) {
-                if (!$("#tenthousand").hasClass("unlocked")) {
-                    alert("You unlocked a banana mansion!");
-                    $("#locked3").css("display", "none");
-                    $("#tenthousand").removeClass("locked").addClass("unlocked");
-                    var image = $("<img>");
-                    image.attr("width", imageWidth);
-                    image.attr("src", "https://www.indystar.com/gcdn/-mm-/b6a26d09f539fc72865ff52237a993d8e3491d6c/c=0-30-800-482/local/-/media/Indianapolis/NG/2013/10/21/banana-house.jpg?width=800&height=450&fit=crop&format=pjpg&auto=webp");
-                    image.appendTo("#tenthousand");
-                }
-            }
-            if (numClicks >= 1000) {
-                if (!$("#million").hasClass("unlocked")) {
-                    alert("You unlocked a banana themed A380!");
-                    $("#locked4").css("display", "none");
-                    $("#million").removeClass("locked").addClass("unlocked");
-                    var image = $("<img>");
-                    image.attr("width", imageWidth);
-                    image.attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5Bn6zipYS5UIHZnXOi_KqVb1TT8raL3zhMQ&s");
-                    image.appendTo("#million");
-                }
-            }
-            if (numClicks>=1500){
-                if (!$("#bananas").hasClass("unlocked")){
-                    alert("You unlocked 1000000 bananas!")
-                    $("#locked7").css("display", "none");
-                    $("#bananas").removeClass("locked").addClass("unlocked");
-                    var image = $("<img>");
-                    image.attr("width", imageWidth);
-                    image.attr("src", "https://images.newscientist.com/wp-content/uploads/2019/01/31134057/gettyimages-86304874.jpg?crop=4:3,smart&width=1200&height=900&upscale=true")
-                    image.appendTo("#bananas");
-                }
-            }
-        });
+    if (numClicks >= 250) {
+        if (!$("#crocpet").hasClass("unlocked")) {
+            alert("You unlocked golden banana!");
+            $("#locked2").css("display", "none");
+            $("#crocpet").removeClass("locked").addClass("unlocked");
+            var image = $("<img>");
+            image.attr("width", imageWidth);
+            image.attr("src", "https://media.artsper.com/artwork/2181373_1_m.jpg");
+            image.appendTo("#crocpet");
+        }
+    }
+    if (numClicks >= 500) {
+        if (!$("#tenthousand").hasClass("unlocked")) {
+            alert("You unlocked a banana mansion!");
+            $("#locked3").css("display", "none");
+            $("#tenthousand").removeClass("locked").addClass("unlocked");
+            var image = $("<img>");
+            image.attr("width", imageWidth);
+            image.attr("src", "https://www.indystar.com/gcdn/-mm-/b6a26d09f539fc72865ff52237a993d8e3491d6c/c=0-30-800-482/local/-/media/Indianapolis/NG/2013/10/21/banana-house.jpg?width=800&height=450&fit=crop&format=pjpg&auto=webp");
+            image.appendTo("#tenthousand");
+        }
+    }
+    if (numClicks >= 1000) {
+        if (!$("#million").hasClass("unlocked")) {
+            alert("You unlocked a banana themed A380!");
+            $("#locked4").css("display", "none");
+            $("#million").removeClass("locked").addClass("unlocked");
+            var image = $("<img>");
+            image.attr("width", imageWidth);
+            image.attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5Bn6zipYS5UIHZnXOi_KqVb1TT8raL3zhMQ&s");
+            image.appendTo("#million");
+        }
+    }
+    if (numClicks >= 1500) {
+        if (!$("#bananas").hasClass("unlocked")) {
+            alert("You unlocked 1000000 bananas!");
+            $("#locked7").css("display", "none");
+            $("#bananas").removeClass("locked").addClass("unlocked");
+            var image = $("<img>");
+            image.attr("width", imageWidth);
+            image.attr("src", "https://images.newscientist.com/wp-content/uploads/2019/01/31134057/gettyimages-86304874.jpg?crop=4:3,smart&width=1200&height=900&upscale=true");
+            image.appendTo("#bananas");
+        }
+    }
+    updateDisplay();
+});

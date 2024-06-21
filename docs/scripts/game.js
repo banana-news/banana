a4b25f905d6fe628d1e92c1410715f0941c0ac23
Albert Kemp <91766342+albertkemp@users.noreply.github.com>
2024-06-21 11:09:39 +1000

    news page

--- docs/scripts/game.js
+++ docs/scripts/game.js
@@ -1,4 +1,4 @@
-const canvas = <document = getElementById('gameCanvas');
+const canvas = document = getElementById('gameCanvas');
 const ctx = canvas.getContent('2d');
 
 function gameloop () {requestAnimation(gameloop);}
